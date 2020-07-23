7eb6f20fe2e8a387a6f1061f02b4c1e3684f85bc
Bea <beamaia.work@gmail.com>
2020-07-22 22:49:54 -0300

    Creating train_model function

--- intro_resnet/resnet.py
+++ intro_resnet/resnet.py
@@ -1,85 +1,50 @@
-# An implementation of https://arxiv.org/pdf/1512.03385.pdf 
-
+from sklearn.metrics import accuracy_score
 import torch
-import torch.nn as nn
-import torchvision.transforms as transforms
-from torch.autograd import Variable
+from torch import nn
+import numpy as np
 
-def conv3x3(in_channels, out_channels, stride=1):
-    return nn.Conv2d(in_channels,
-                     out_channels,
-                     kernel_size=3,
-                     stride=stride,
-                     padding=1,
-                     bias=False)
+def update_lr(optimizer, lr):    
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
 
-class ResidualBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, stride=1, downsample=None):
-        super(ResidualBlock, self).__init__()
-        self.conv1 = conv3x3(in_channels, out_channels, stride)
-        self.bn1 = nn.BatchNorm2d(out_channels)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3(out_channels, out_channels)
-        self.bn2 = nn.BatchNorm2d(out_channels)
-        self.downsample = downsample
-    
-    def forward(self, x):
-        residual = x
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-        out = self.conv2(out)
-        out = self.bn2(out)
-        
-        if self.downsample:
-            residual = self.downsample(x)
-            
-        out += residual
-        out = self.relu(out)
-        
-        return out
+def train_model(model, data_loader, device):
+    model.train()
+    num_epochs = int(input("Number of epochs: "))
+    learning_rate = float(input("Learning rate: "))
 
-class ResNet(nn.Module):
-    def __init__(self, block, layers, num_classes=10):
-        super(ResNet, self).__init__()
-        self.in_channels = 16
-        self.conv = conv3x3(3, 16)  # 1 when using mnist, 3 when using cifar10
-        self.bn = nn.BatchNorm2d(16)
-        self.relu = nn.ReLU(inplace=True)
-        self.layer1 = self.make_layer(block, 16, layers[0])
-        self.layer2 = self.make_layer(block, 32, layers[1], 2)
-        self.layer3 = self.make_layer(block, 64, layers[2], 2)
-        self.avg_pool = nn.AvgPool2d(8)
-        self.fc = nn.Linear(64, num_classes)
+    error = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+    for epoch in range(num_epochs):
+        print(epoch)
+        print(f'Epoch {epoch + 1} of {num_epochs}.')
+        count = 0
+ 
+        for images, labels in data_loader:
+            images.resize_([100,1,28,28]).to(device)
+            labels.to(device)
+
+            # Forward pass
+            outputs = model(images)
+            # outputs = torch.max(outputs.data, 1)[1].cpu().numpy()
+
+            loss = error(outputs, labels)
+
+            # Backward and optimize
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            if (count + 1) % 100 == 0:
+                print (f'Step [{count + 1}/{len(data_loader)}] Loss: {loss.item()}')
+
+            count += 1
         
-    
-    def make_layer(self, block, out_channels, blocks, stride=1):
-        
-        downsample = None
-        
-        if (stride != 1) or (self.in_channels != out_channels):
-            downsample = nn.Sequential(
-                conv3x3(self.in_channels, out_channels, stride=stride),
-                nn.BatchNorm2d(out_channels))
-        
-        layers = []
-        layers.append(block(self.in_channels, out_channels, stride, downsample))
-        self.in_channels = out_channels
-        
-        for i in range(1, blocks):
-            layers.append(block(out_channels, out_channels))
-        
-        return nn.Sequential(*layers)
-    
-    def forward(self, x):
-        out = self.conv(x)
-        out = self.bn(out)
-        out = self.relu(out)
-        out = self.layer1(out)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.avg_pool(out)
-        out = out.view(out.size(0), -1)
-        out = self.fc(out)
-        
-        return out
+        # change_learning_rate = int(input("Change learning rate?\n0: No   1: Yes"))
+
+        if epoch % 20 == 0:
+            learning_rate /= 3
+            update_lr(optimizer, learning_rate)
+
+    return model 
+
