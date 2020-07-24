f866f4a050a0b744f16661a1ab7884ce6307920d
Bea <beamaia.work@gmail.com>
2020-07-23 23:24:01 -0300

    Trying to modify ResNet for grayscale

--- intro_resnet/mnist_resnet.py
+++ intro_resnet/mnist_resnet.py
@@ -1,20 +1,32 @@
-from torchvision.models.resnet import ResNet, BasicBlock
-import torch
+from torchvision import models
+from torchvision.models.resnet import ResNet, Bottleneck
+import torch.nn as nn
 
 class MnistResNet(ResNet):
-    def __init__(self):
-        super(MnistResNet, self).__init__(BasicBlock, [3, 4, 6, 3], num_classes = 10)
-        self.conv1 = torch.nn.Conv2d(1, 64, 
-                                     kernel_size = (7,7), 
-                                     stride=(2,2), 
-                                     padding=(3,3), bias=False)
+    def __init__(self, block,layers,num_classes, pretrained=False, bias=False):
+        super(MnistResNet, self).__init__(block = Bottleneck, layers=[3, 4, 6, 3])
+        self.conv1 = nn.Conv2d(1, 
+                               64, 
+                               kernel_size = 3, 
+                               stride=2, 
+                               padding=3, 
+                               bias=False)
+        
+def resnet50(num_classes=10):
+    model = MnistResNet(block=models.resnet.Bottleneck, 
+                        layers=[3, 4, 6, 3], 
+                        num_classes = 10, 
+                        pretrained=False, 
+                        bias=False)
+
+    return model
 
 if  __name__ == "__main__":
     import resnet
-    import dataset_mnist as  mnist
+    import mnist_dataset as  mnist
     
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     train_loader, test_loader = mnist.create_DataLoader() 
     model = MnistResNet() 
-    accuracy_scores = resnet.train_model(model, train_loader, device)          
+    resnet.train_model(model, train_loader, device)          
